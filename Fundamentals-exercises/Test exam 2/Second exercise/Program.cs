f44886e90015807cd95fdb7d564e1a8526c11eb2
Sechkin <surenderordie@abv.bg>
2022-03-30 16:56:53 +0300

    This exercise finds All the cool emojies using regex(the cool threshhold is the sum of all the digits in the text)

---
+++ Fundamentals-exercises/Test exam 2/Second exercise/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Second_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Regex emojiPattern = new Regex(@"(::|\*\*)(?<word>[A-Z][a-z]{2,})\1");
+            Regex numberPattern = new Regex(@"\d");
+            List<long> coolThresholdList = numberPattern.Matches(text).Select(x => x.Value).Select(x => long.Parse(x)).ToList();
+            long coolThreshold = 1;
+            foreach (long element in coolThresholdList)
+            {
+                coolThreshold *= element;
+
+
+
+            }
+            MatchCollection matches = emojiPattern.Matches(text);
+            List<string> emojies = new List<string>();
+            foreach (Match match in matches)
+            {
+                string emoji = match.Groups["word"].Value;
+                int coolness = 0;
+                foreach (char character in emoji)
+                {
+                    coolness += character;
+
+
+
+                }
+                if (coolness >= coolThreshold)
+                {
+                    emojies.Add(match.Value);
+
+
+
+                }
+
+
+
+            }
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+            foreach (string emoji in emojies)
+            {
+                Console.WriteLine(emoji);
+
+
+
+            }
+
+        }
+    }
+}
