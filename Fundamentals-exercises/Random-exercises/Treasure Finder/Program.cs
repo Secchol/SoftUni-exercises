28834dd8e53ab2982c89df82cd2472807eb7d8a3
Sechkin <surenderordie@abv.bg>
2022-04-01 11:43:14 +0300

    This exercise has a list of keys that we have to substract from the ASCII codes of chars in a string and after we rework the text we find the treasure and its coordinates

---
+++ Fundamentals-exercises/Random-exercises/Treasure Finder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] keys = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            string text = Console.ReadLine();
+            while (text != "find")
+            {
+                int key = 0;
+                StringBuilder workedText = new StringBuilder();
+                for (int i = 0; i < text.Length; i++, key++)
+                {
+                    workedText.Append((char)(text[i] - keys[key]));
+                    if (key == keys.Length - 1)
+                    {
+                        key = -1;
+                    }
+                }
+                string reworkedText = workedText.ToString();
+                string treasureType = reworkedText.Substring(reworkedText.IndexOf('&') + 1, reworkedText.LastIndexOf('&') - reworkedText.IndexOf('&') - 1);
+                string coordinates = reworkedText.Substring(reworkedText.IndexOf('<') + 1, reworkedText.IndexOf('>') - reworkedText.IndexOf('<') - 1);
+                Console.WriteLine($"Found {treasureType} at {coordinates}");
+
+
+                text = Console.ReadLine();
+            }
+        }
+    }
+}
