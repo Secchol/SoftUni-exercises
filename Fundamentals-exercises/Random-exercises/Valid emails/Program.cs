0d666f89ab705ec3df6313cd107751b40aa0e87a
Sechkin <surenderordie@abv.bg>
2022-04-03 16:55:25 +0300

    Using regex to validate emails

---
+++ Fundamentals-exercises/Random-exercises/Valid emails/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Valid_emails
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(?<=\s)([a-z]+|\d+)(\d+|\w+|\.+|-+)([a-z]+|\d+)\@[a-z]+\-?[a-z]+\.[a-z]+(\.[a-z]+)?";
+            List<string> validEmails = Regex.Matches(text, pattern).Select(x => x.Value).Select(x => x.Trim()
+            ).ToList();
+            Console.WriteLine(String.Join(Environment.NewLine, validEmails));
+
+
+        }
+    }
+}
+
