2395ad5edb0475be328517965c9585fe758ec4ea
Sechkin <surenderordie@abv.bg>
2022-04-06 16:29:00 +0300

    Pascal triangle

---
+++ Fundamentals-exercises/Random-exercises/Pascal triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pascal_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[] array = { 1 };
+
+            for (int i = 1; i <= rows; i++)
+            {
+                Console.WriteLine(String.Join(" ", array));
+                int[] arrayToModify = new int[i + 1];
+                arrayToModify[0] = 1;
+                arrayToModify[arrayToModify.Length - 1] = 1;
+                int j = 0;
+                int position = 1;
+                while (j < array.Length - 1)
+                {
+                    arrayToModify[position] = array[j] + array[j + 1];
+                    position++;
+                    j++;
+
+                }
+                array = arrayToModify;
+            }
+        }
+    }
+}
