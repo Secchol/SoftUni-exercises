bfe9ce6b7e63cedcafd6b203714cb2395a4a1364
Sechkin <surenderordie@abv.bg>
2022-04-22 12:57:04 +0300

    This exercise finds all the numbers that are equal to the sum of their cubed digits

---
+++ Fundamentals-exercises/Random-exercises/I3-8v-22/I3-8v-22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace I3_8v_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача И3-34. /КУБОВЕ НА ЦИФРИТЕ/ Да се състави програма за намиране на всички естествени числа, равни на сумата от кубовете на цифрите си.
+            // Изготвил Сечкин Чолак от 8в клас
+            Console.WriteLine("This program finds all the natural numbers that are equal to the sum of all its cubed digits.");
+            
+
+            for (int i = 1; i <= 407; i++)
+            {
+                int number = i;
+                int sum = 0;
+                while (number != 0)
+                {
+                    int digit = number % 10;
+                    sum += digit * digit * digit;
+                    number /= 10;
+
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+
+
+                }
+
+            }
+            Console.WriteLine("An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the");
+            Console.WriteLine("number itself. There are just five numbers which are the sums of the cubes of their digits.");
+
+
+
+        }
+    }
+}
