b58da6e55401dbf1293759b431a3b9c7b022587d
Sechkin <surenderordie@abv.bg>
2022-05-11 19:10:14 +0300

    This exercise prints all the expressions between the brackets

---
+++ C# advanced/Stacks and Queues/Arithmetic Operations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arithmetic_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+
+
+                }
+                else if (expression[i] == ')')
+                {
+                    int firstIndex = stack.Pop();
+                    int lastIndex = i;
+                    Console.WriteLine(expression.Substring(firstIndex, lastIndex - firstIndex + 1));
+
+
+                }
+            }
+        }
+    }
+}
