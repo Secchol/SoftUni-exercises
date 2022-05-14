b386f5a490c21effe3eaa99373d18ad75ce298ab
Sechkin <surenderordie@abv.bg>
2022-05-14 15:32:52 +0300

    This exercise plays songs untill there are no more songs in the queue

---
+++ C# advanced/Clothes shop/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Clothes_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesWeight = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int racksCount = 1;
+            int currentWeightOnRack = 0;
+            if (rackCapacity == 0)
+            {
+                Console.WriteLine(clothesWeight.Count);
+                return;
+
+
+            }
+
+
+            while (clothesWeight.Count > 0)
+            {
+                currentWeightOnRack += clothesWeight.Peek();
+                if (currentWeightOnRack == rackCapacity)
+                {
+                    currentWeightOnRack = 0;
+                    racksCount++;
+                    clothesWeight.Pop();
+
+
+                }
+                else if (currentWeightOnRack > rackCapacity)
+                {
+                    currentWeightOnRack = 0;
+                    racksCount++;
+
+
+
+                }
+                else
+                {
+                    clothesWeight.Pop();
+
+
+                }
+
+
+
+            }
+            Console.WriteLine(racksCount);
+        }
+    }
+}
