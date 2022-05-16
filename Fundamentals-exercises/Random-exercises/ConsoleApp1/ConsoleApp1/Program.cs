c96f2d160d5c0577238ce817506da8f9530dfc4b
Sechkin <surenderordie@abv.bg>
2022-05-16 17:03:01 +0300

    1

--- Fundamentals-exercises/Random-exercises/ConsoleApp1/ConsoleApp1/Program.cs
+++ Fundamentals-exercises/Random-exercises/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,41 +1,41 @@
-﻿using System;
+﻿
 
-namespace ConsoleApp1
+
+using System;
+
+namespace ConsoleApplication1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            int g = 1990;// начабна година
-            double a = 20;// начална реколта
-            double sum = 20;// обща реколта
-            int c = 0;// годината когато реклотата пада под 5 тона
-            bool foundYear = false;//гледа дали вече не е намерена годината когато общата реколта става за първи път над 90 тона
-            while (a > 5)
+            int i = int.Parse(Console.ReadLine());
+            int i1 = int.Parse(Console.ReadLine());
+            int i2 = int.Parse(Console.ReadLine());
+            int i3 = int.Parse(Console.ReadLine());
+            int i4 = int.Parse(Console.ReadLine());
+            int i5 = int.Parse(Console.ReadLine());
+            int vs = i1 + i2 + i3 + i4 + i5;
+            double dena;
+            double cena;
+            if (vs >= 50)
             {
-                g++;
-                if (g % 2 == 0)
-                {
-                    a = 0.8 * a;
-
-
-                }
-                sum = sum + a;
-                if (sum > 90 && foundYear == false)
-                {
-                    c = g;
-                    foundYear = true;
-
-                }
-                
-
-
+                cena = 0.75 * (i1 * 2.60 + i2 * 3 + i3 * 4.10 + i4 * 8.20 + i5 * 2);
+                dena = cena * 0.9;
             }
-            Console.WriteLine(c.To);//изписва годината когато общата реколта става над 90 тона
-            Console.WriteLine(g);//изписва годината когато добивната реколта става под 5 тона
-
-
-
+            else
+            {
+                cena = i1 * 2.60 + i2 * 3 + i3 * 4.10 + i4 * 8.20 + i5 * 2;
+                dena = cena * 0.9;
+            }
+            if (dena >= i)
+            {
+                Console.WriteLine("Yes! " + Math.Round((dena - i), 2) + " lv left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money! " + Math.Round((i - dena), 2) + " lv needed.");
+            }
         }
     }
 }
