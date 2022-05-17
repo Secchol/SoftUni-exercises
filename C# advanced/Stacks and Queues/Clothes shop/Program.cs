a6d36803e22e70ce3ebf05cf27ecc1644fff9278
Sechkin <surenderordie@abv.bg>
2022-05-17 15:40:08 +0300

    Putting files into one folder

---
+++ C# advanced/Stacks and Queues/Clothes shop/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Clothes_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int racksCount = 1;
+            int currentWeightOnRack = 0;
+
+
+
+            while (clothes.Count > 0)
+            {
+                currentWeightOnRack += clothes.Peek();
+                if (currentWeightOnRack == rackCapacity)
+                {
+                    clothes.Pop();
+                    currentWeightOnRack = 0;
+                    if (clothes.Count > 0)
+                    {
+
+                        racksCount++;
+
+                    }
+                    
+
+
+
+                }
+                else if (currentWeightOnRack > rackCapacity)
+                {
+                    currentWeightOnRack = 0;
+                    racksCount++;
+
+
+
+                }
+                else
+                {
+                    clothes.Pop();
+
+
+                }
+
+
+
+            }
+            Console.WriteLine(racksCount);
+        }
+    }
+}
---
+++ C# advanced/Stacks and Queues/Songs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries));
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+                if (command == "Play")
+                {
+                    queue.Dequeue();
+
+
+
+                }
+                else if (command.Substring(0, 3) == "Add")
+                {
+                    string song = command.Substring(3, command.Length - 3).Trim();
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+
+
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+
+
+                    }
+
+
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queue));
+
+
+
+                }
+
+
+
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
