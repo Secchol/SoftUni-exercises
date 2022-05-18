7c78a0fcce3f9a666b9af1bbaa8130c135c98c10
Sechkin <surenderordie@abv.bg>
2022-05-18 17:46:52 +0300

    this program fins out if a certain amount of bottles will be able to fill up cups and prints the wasted water in the process

---
+++ C# advanced/Stacks and Queues/Cups and Bottles/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cups_and_Bottles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> cupsCapacity = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            Stack<int> bottlesCapacity = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+            int wastedWater = 0;
+            while (cupsCapacity.Count > 0 && bottlesCapacity.Count > 0)
+            {
+                int currentCupCapacity = cupsCapacity.Peek();
+                int currentBottleCapacity = bottlesCapacity.Peek();
+                if (currentBottleCapacity >= currentCupCapacity)
+                {
+                    currentBottleCapacity -= currentCupCapacity;
+                    wastedWater += currentBottleCapacity;
+                    bottlesCapacity.Pop();
+                    cupsCapacity.Dequeue();
+
+
+                }
+                else if (currentCupCapacity > currentBottleCapacity)
+                {
+                    currentCupCapacity -= currentBottleCapacity;
+                    bottlesCapacity.Pop();
+                    while (true)
+                    {
+                        if (bottlesCapacity.Peek() - currentCupCapacity < 0)
+                        {
+                            currentCupCapacity -= bottlesCapacity.Pop();
+
+
+
+                        }
+                        else
+                        {
+                            wastedWater += bottlesCapacity.Pop() - currentCupCapacity;
+                            cupsCapacity.Dequeue();
+                            break;
+
+
+                        }
+
+
+
+
+
+
+                    }
+
+
+
+                }
+            }
+            if (bottlesCapacity.Count == 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupsCapacity)}");
+
+
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottlesCapacity)}");
+
+
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
