1002f6e7ecf32283fba9fc06dd0cd838ba9c654e
Sechkin <surenderordie@abv.bg>
2022-05-20 14:19:31 +0300

    This program finds the count of 2x2 equal character matrices in a bigger matrice

---
+++ C# advanced/Multidimensional and jagged arrays and matrices/2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _2X2_Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+            for (int row = 0; row < matrixSize[0]; row++)
+            {
+                char[] rowElements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => char.Parse(x))
+                .ToArray();
+                for (int col = 0; col < matrixSize[1]; col++)
+                {
+                    matrix[row, col] = rowElements[col];
+                }
+            }
+            int equalSquaresCount = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col + 1] == matrix[row + 1, col] && matrix[row + 1, col] == matrix[row + 1, col + 1])
+                    {
+                        equalSquaresCount++;
+
+
+
+                    }
+                }
+
+
+
+            }
+            
+                Console.WriteLine(equalSquaresCount);
+
+
+            
+
+        }
+    }
+}
