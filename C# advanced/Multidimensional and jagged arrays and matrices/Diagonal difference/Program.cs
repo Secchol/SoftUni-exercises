1ccb28a6c9fa932c5c21b381e6455c0861bc5b0a
Sechkin <surenderordie@abv.bg>
2022-05-20 13:35:08 +0300

    This program finds the absolute difference of the two top triangles of a matrix

---
+++ C# advanced/Multidimensional and jagged arrays and matrices/Diagonal difference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] rowElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = rowElements[col];
+                }
+
+
+            }
+            int firstDiagonalSum = 0;
+            int currentCol = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++, currentCol++)                    firstDiagonalSum += matrix[row, currentCol];
+            
+            currentCol = matrix.GetLength(0) - 1;
+            int secondDiagonalSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++, currentCol--)
+                            secondDiagonalSum += matrix[row, currentCol];
+            
+            Console.WriteLine(Math.Abs(firstDiagonalSum - secondDiagonalSum));
+        }
+    }
+}
