fc02ed6bea82959e0f3d399d52b72f7b029ac3db
Sechkin <surenderordie@abv.bg>
2022-05-25 17:01:04 +0300

    This program counts all the pieces of clothing in a wardrobe and finds the wanted one

--- C# advanced/Sets and Dictionaries-advanced/Wardrobe/Program.cs
+++ C# advanced/Sets and Dictionaries-advanced/Wardrobe/Program.cs
@@ -8,7 +8,48 @@ namespace Wardrobe
     {
         static void Main(string[] args)
         {
-            int count = int.Parse(Console.ReadLine);
+            int count = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 0; i < count; i++)
+            {
+                string[] token = Console.ReadLine()
+                    .Split(" -> ");
+                string color = token[0];
+                if (!wardrobe.ContainsKey(color))
+                    wardrobe[color] = new Dictionary<string, int>();
+                string[] clothes = token[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string pieceOfClothing in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(pieceOfClothing))
+                        wardrobe[color][pieceOfClothing] = 0;
+                    wardrobe[color][pieceOfClothing]++;
+
+
+                }
+
+
+
+            }
+            string[] clothesToSearchFor = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var (color, clothing) in wardrobe)
+            {
+                
+                Console.WriteLine($"{color} clothes:");
+                foreach (var cloth in clothing)
+                {
+                    Console.Write($"* {cloth.Key} - {cloth.Value}");
+                    if (color == clothesToSearchFor[0] && cloth.Key == clothesToSearchFor[1])
+                        Console.Write(" (found!)");
+                    Console.WriteLine();
+
+
+
+                }
+
+
+            }
         }
     }
 }
