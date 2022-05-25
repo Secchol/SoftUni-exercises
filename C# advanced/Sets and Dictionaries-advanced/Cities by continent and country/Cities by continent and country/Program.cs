4d5b3209e53065fa3713ed8b0ccb7bfb3711af40
Sechkin <surenderordie@abv.bg>
2022-05-25 14:20:08 +0300

    This program finds if there are any cars left in a parking lot

---
+++ C# advanced/Sets and Dictionaries-advanced/Cities by continent and country/Cities by continent and country/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cities_by_continent_and_country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            var cities = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < count; i++)
+            {
+                string[] token = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = token[0];
+                string country = token[1];
+                string city = token[2];
+                if (!cities.ContainsKey(continent))
+                    cities[continent] = new Dictionary<string, List<string>>();
+                if (!cities[continent].ContainsKey(country))
+                    cities[continent][country] = new List<string>();
+                cities[continent][country].Add(city);
+
+            }
+            foreach (var (continent, countries) in cities)
+            {
+                Console.WriteLine($"{continent}:");
+                foreach (var country in countries)
+                {
+                    string towns = string.Join(", ",country.Value);
+                    Console.WriteLine($"\t{country.Key} -> {towns}");
+
+
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ C# advanced/Sets and Dictionaries-advanced/Parking Lot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] commandsArray = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string action = commandsArray[0];
+                string carNumber = commandsArray[1];
+                if (action == "IN")
+                    cars.Add(carNumber);
+                else if (action == "OUT" && cars.Contains(carNumber))
+                    cars.Remove(carNumber);
+
+                command = Console.ReadLine();
+            }
+            if (cars.Count == 0)           
+                Console.WriteLine("Parking Lot is Empty");          
+            else
+            {
+                foreach (string car in cars)
+                    Console.WriteLine(car);
+
+
+            }
+        }
+    }
+}
