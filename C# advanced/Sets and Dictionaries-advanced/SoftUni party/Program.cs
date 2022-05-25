8245ba6e21b8d0b0d2a04b96d8d5a1b595b9ca6e
Sechkin <surenderordie@abv.bg>
2022-05-25 16:41:12 +0300

    A couple of exercises

---
+++ C# advanced/Sets and Dictionaries-advanced/SoftUni party/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            string command = Console.ReadLine();
+            bool party = false;
+            while (command != "END")
+            {
+                if (command == "PARTY")
+                {
+                    party = true;
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (party)
+                {
+                    if (VIP.Contains(command))
+                        VIP.Remove(command);
+                    else
+                        regular.Remove(command);
+
+
+                }
+                else
+                {
+                    if (char.IsDigit(command[0]))
+                        VIP.Add(command);
+                    else
+                        regular.Add(command);
+
+
+                }
+
+                command = Console.ReadLine();
+
+
+
+
+            }
+            Console.WriteLine(VIP.Count + regular.Count);
+            foreach (string richPerson in VIP)
+                Console.WriteLine(richPerson);
+            foreach (string poorPerson in regular)
+                Console.WriteLine(poorPerson);
+        }
+    }
+}
---
+++ C# advanced/Sets and Dictionaries-advanced/Wardrobe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine);
+        }
+    }
+}
