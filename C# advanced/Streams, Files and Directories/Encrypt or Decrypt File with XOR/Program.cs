36ba85b7056df4c969e85778fdbbbe094cc4adb7
Sechkin <surenderordie@abv.bg>
2022-05-29 12:12:37 +0300

    This program encypts an image using the xor operator

---
+++ C# advanced/Streams, Files and Directories/Encrypt or Decrypt File with XOR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Encrypt_or_Decrypt_File_with_XOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pic = new FileStream("../../../NewFolder/picture.jpg",FileMode.Open))
+            {
+                using (var encPic = new FileStream("../../../example-encrypted.jpg", FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    while (true)
+                    {
+                        int bytesRead = pic.Read(buffer);
+                        if (bytesRead == 0) break;
+                        const byte secret = 183;
+                        for (int i = 0; i < bytesRead; i++)
+                            buffer[i] = (byte)(buffer[i] ^ secret);
+                        encPic.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+
+        }
+    }
+}
