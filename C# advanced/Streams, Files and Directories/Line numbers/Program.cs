d657854803853a79ca92670f3a05bc055be55ea8
Sechkin <surenderordie@abv.bg>
2022-05-29 10:30:23 +0300

    This program reads a text file and numbers its lines in another file

---
+++ C# advanced/Streams, Files and Directories/Line numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader input = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter output = new StreamWriter(outputFilePath))
+                {
+                    string line = input.ReadLine();
+                    int lineCounter = 1;
+                    while (line != null)
+                    {
+                        output.WriteLine($"{lineCounter}. {line}");
+                        lineCounter++;
+                        line = input.ReadLine();
+                    }
+
+
+                }
+
+
+
+            }
+        }
+    }
+}
+
