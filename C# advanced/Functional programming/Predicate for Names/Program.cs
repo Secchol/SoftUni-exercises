4e7b56a78af144f9b825fa5fe2ab002d42c42258
Sechkin <surenderordie@abv.bg>
2022-06-01 23:29:04 +0300

    This program finds the names in a list that have an equal or lower length to the given one

---
+++ C# advanced/Functional programming/Predicate for Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            Predicate<string> validLength = x => x.Length <= nameLength;
+            names = names.FindAll(validLength);
+            Console.WriteLine(String.Join("\n", names));
+        }
+    }
+}
