17fe32e140fb22835bb3ab7ca2cca382a3a70f89
Sechkin <surenderordie@abv.bg>
2022-06-02 00:01:23 +0300

    This program finds all the numbers in a given range that can be divided by integers in an array

---
+++ C# advanced/Functional programming/List of Predicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace List_of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= range; i++)
+            {
+                numbers.Add(i);
+            }
+            Func<int, int[], bool> isDivisible = (x, dividers) =>
+              {
+                  foreach (int divider in dividers)
+                  {
+                      if (x % divider != 0)
+                          return false;
+
+
+                  }
+                  return true;
+
+
+
+              };
+            numbers = numbers.Where(x => isDivisible(x, dividers)).ToList();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
