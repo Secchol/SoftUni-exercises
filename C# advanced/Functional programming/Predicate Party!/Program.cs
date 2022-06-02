d46884fe5a3d8a4e8b6a3f4cc298350314580fd9
Sechkin <surenderordie@abv.bg>
2022-06-02 16:03:35 +0300

    This program finds all the people that are coming to a party

---
+++ C# advanced/Functional programming/Predicate Party!/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command = Console.ReadLine();
+            while (command != "Party!")
+            {
+                string[] commandArray = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Predicate<string> format = FormatFinder(commandArray[1], commandArray[2]);
+
+                if (commandArray[0] == "Remove")
+                {
+                    people.RemoveAll(format);
+
+
+                }
+                else if (commandArray[0] == "Double")
+                {
+                    for (int i = 0; i < people.Count; i++)
+                    {
+                        if (format(people[i]))
+                        {
+                            people.Insert(i + 1, people[i]);
+                            i += 1;
+
+
+
+                        }
+                    }
+
+
+                }
+                command = Console.ReadLine();
+
+            }
+            if (people.Count == 0)
+            {
+                Console.WriteLine("Nobody is going to the party!");
+                Environment.Exit(0);
+
+            }
+            Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+
+
+
+
+
+        }
+        static Predicate<string> FormatFinder(string condition, string element)
+        {
+            switch (condition)
+            {
+                case "StartsWith":
+                    return x => x.StartsWith(element);
+                case "EndsWith":
+                    return x => x.EndsWith(element);
+                case "Length":
+                    return x => x.Length == int.Parse(element);
+                default:
+                    return null;
+
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
+
