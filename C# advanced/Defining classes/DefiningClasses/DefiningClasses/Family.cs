a98b115c0d1a24ed1f668305b166f062d5d2f9bf
Sechkin <surenderordie@abv.bg>
2022-06-05 09:54:47 +0300

    This program can find the people from a list whose age is above 30 and sorts them in alphabetical order

---
+++ C# advanced/Defining classes/DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+        public List<Person> People { get { return people; } set { this.people = value; } }
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+
+
+        }
+
+        public string GetOldestMember()
+        {
+            Person oldestPerson = new Person();
+            int maxAge = int.MinValue;
+            foreach (Person person in people)
+            {
+                if (person.Age > maxAge)
+                {
+                    maxAge = person.Age;
+                    oldestPerson = person;
+
+
+                }
+
+
+            }
+            string[] personInfo = new string[] { oldestPerson.Name, oldestPerson.Age.ToString() };
+            return string.Join(" ", personInfo);
+
+
+
+        }
+    }
+}
