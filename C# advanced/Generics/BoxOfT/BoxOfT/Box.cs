030f4f9fb8169380ea08a6c585756a8a1491442e
Sechkin <surenderordie@abv.bg>
2022-06-09 16:42:44 +0300

    This program implements a generic class Box that can add and remove elements from a collection

---
+++ C# advanced/Generics/BoxOfT/BoxOfT/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> Elements { get; set; }
+        public int Count { get; private set; }
+        public Box()
+        {
+            this.Elements = new List<T>();
+            Count = 0;
+
+
+        }
+        public void Add(T element)
+        {
+            this.Elements.Add(element);
+            Count++;
+
+
+        }
+        public T Remove()
+        {
+            T removedElement = Elements[Count - 1];
+            Elements.RemoveAt(--Count);
+
+            return removedElement;
+
+
+        }
+
+    }
+}
