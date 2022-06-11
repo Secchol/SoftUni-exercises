ae895b2fa29ea58a5c45ac911420ac78dfa6c0e2
Sechkin <surenderordie@abv.bg>
2022-06-11 09:06:57 +0300

    This program creates a custom tuple class

---
+++ C# advanced/Generics/MyTuple/MyTuple/MyTuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTuple
+{
+    public class MyTuple<TFirst, TSecond, TThird>
+    {
+        public MyTuple(TFirst first, TSecond second, TThird third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+
+
+
+        }
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+        public TThird Third { get; set; }
+        public override string ToString()
+        {
+            return $"{this.First} -> {this.Second} -> {this.Third}";
+        }
+    }
+}
---
+++ C# advanced/Generics/MyTuple/MyTuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyTuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split();
+            string names = $"{personInfo[0]} {personInfo[1]}";
+            string street = personInfo[2];
+            string city = personInfo.Length > 4 ? $"{personInfo[3]} {personInfo[4]}" : $"{personInfo[3]}";
+            string[] alcoholicInfo = Console.ReadLine().Split();
+            string name = alcoholicInfo[0];
+            int age = int.Parse(alcoholicInfo[1]);
+            string state = alcoholicInfo[2];
+            if (state == "drunk")
+            {
+                state = "True";
+
+            }
+            else
+            {
+                state = "False";
+
+            }
+            string[] bankInfo = Console.ReadLine().Split();
+            string personName = bankInfo[0];
+            double balance = double.Parse(bankInfo[1]);
+            string bankName = bankInfo[2];
+            MyTuple<string, string, string> firstTuple = new MyTuple<string, string, string>(names, street, city);
+            MyTuple<string, int, string> secondTuple = new MyTuple<string, int, string>(name, age, state);
+            MyTuple<string, double, string> thirdTuple = new MyTuple<string, double, string>(personName, balance, bankName);
+            Console.WriteLine(firstTuple.ToString());
+            Console.WriteLine(secondTuple.ToString());
+            Console.WriteLine(thirdTuple.ToString());
+        }
+    }
+}
