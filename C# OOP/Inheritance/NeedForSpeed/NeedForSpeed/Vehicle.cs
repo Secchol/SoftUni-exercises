813e8ed0d3b32a8225a534c33b99299df5cc6680
Secchol <102237850+Secchol@users.noreply.github.com>
2022-08-19 21:20:45 +0300

    Add files via upload

---
+++ C# OOP/Inheritance/NeedForSpeed/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        
+
+        public const double DefaultFuelConsumption = 1.25;
+        public virtual double FuelConsumption { get; set; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+            FuelConsumption = DefaultFuelConsumption;
+        }
+        public virtual void Drive(double kilometers)
+        {
+            if (Fuel - (kilometers * FuelConsumption) >= 0)
+                Fuel -= kilometers * FuelConsumption;
+
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Person/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get { return name; } set { name = value; } }
+        public virtual int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+
+                }
+                
+            }
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {Name}, Age: {Age}");
+            return sb.ToString();
+        }
+    }
+}
