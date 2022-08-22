f8f14fac08498ea8a65d2d4c4c7b8ebf80193251
Secchol <102237850+Secchol@users.noreply.github.com>
2022-08-22 09:53:42 +0300

    Add files via upload

---
+++ C# OOP/Intefaces and Abstraction/Telephony/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Smartphone smartphone = new Smartphone();
+            StationaryPhone stationeryPhone = new StationaryPhone();
+            string[] phoneNumbers = Console.ReadLine().Split();
+            string[] websites = Console.ReadLine().Split();
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                if (phoneNumber.ToCharArray().Any(x => !char.IsDigit(x)))
+                {
+                    Console.WriteLine("Invalid number!");
+
+                }
+                else if (phoneNumber.Length == 7)
+                {
+                    stationeryPhone.Call(phoneNumber);
+
+                }
+                else
+                {
+                    smartphone.Call(phoneNumber);
+
+                }
+
+
+
+            }
+
+            foreach (string website in websites)
+            {
+                if (website.ToCharArray().Any(x => char.IsDigit(x)))
+                {
+                    Console.WriteLine("Invalid URL!");
+
+                }
+                
+                else
+                {
+                    smartphone.Browse(website);
+
+                }
+
+
+
+            }
+        }
+    }
+}
---
+++ C# OOP/Intefaces and Abstraction/Telephony/Smartphone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICanBrowse, ICanCall
+    {
+        public void Browse(string url)
+        {
+            Console.WriteLine($"Browsing: {url}!");
+        }
+
+        public void Call(string number)
+        {
+            
+            Console.WriteLine($"Calling... {number}");
+        }
+    }
+}
