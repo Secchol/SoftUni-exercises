28782e0262a59f210537b1739b256cab4938abb7
Secchol <102237850+Secchol@users.noreply.github.com>
2022-08-26 21:29:17 +0300

    Add files via upload

---
+++ C# OOP/Exceptions/MoneyTransactions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoneyTransactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] accountsInput = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<int, double> accounts = new Dictionary<int, double>();
+            foreach (string account in accountsInput)
+            {
+                string[] accountInfo = account.Split("-", StringSplitOptions.RemoveEmptyEntries);
+                int accountNumber = int.Parse(accountInfo[0]);
+                double accountBalance = double.Parse(accountInfo[1]);
+                accounts.Add(accountNumber, accountBalance);
+
+
+            }
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                try
+                {
+                    string[] commandsArray = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string action = commandsArray[0];
+                    int accountNumber = int.Parse(commandsArray[1]);
+                    double sum = double.Parse(commandsArray[2]);
+
+                    if (action == "Deposit")
+                    {
+                        AccountChecker(accountNumber, accounts);
+                        accounts[accountNumber] += sum;
+                        Console.WriteLine($"Account {accountNumber} has new balance: {accounts[accountNumber]:F2}");
+                    }
+                    else if (action == "Withdraw")
+                    {
+                        AccountChecker(accountNumber, accounts);
+                        BalanceChecker(sum, accountNumber, accounts);
+                        accounts[accountNumber] -= sum;
+                        Console.WriteLine($"Account {accountNumber} has new balance: {accounts[accountNumber]:F2}");
+
+
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid command!");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+
+                    Console.WriteLine("Enter another command");
+
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+        public static bool AccountChecker(int account, Dictionary<int, double> accounts)
+        {
+            if (!accounts.ContainsKey(account))
+            {
+                throw new ArgumentException("Invalid account!");
+
+            }
+            return true;
+
+
+        }
+        public static bool BalanceChecker(double sum, int account, Dictionary<int, double> accounts)
+        {
+            if (accounts[account] < sum)
+            {
+                throw new ArgumentException("Insufficient balance!");
+
+            }
+            return true;
+
+
+        }
+    }
+}
---
+++ C# OOP/Exceptions/Play catch/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Play_catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int exceptionsCount = 0;
+            while (exceptionsCount != 3)
+            {
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    string action = command[0];
+                    int firstIndex = int.Parse(command[1]);
+                    if (firstIndex < 0 || firstIndex >= numbers.Count)
+                    {
+                        throw new ArgumentException("The index does not exist!");
+
+
+                    }
+                    if (action == "Replace")
+                    {
+                        int element = int.Parse(command[2]);
+                        numbers.RemoveAt(firstIndex);
+                        numbers.Insert(firstIndex, element);
+
+
+                    }
+                    else if (action == "Print")
+                    {
+                        int endIndex = int.Parse(command[2]);
+                        if (endIndex < 0 || endIndex >= numbers.Count)
+                        {
+                            throw new ArgumentException("The index does not exist!");
+
+
+                        }
+                        List<int> numbersToPrint = new List<int>();
+                        for (int i = firstIndex; i <= endIndex; i++)
+                        {
+                            numbersToPrint.Add(numbers[i]);
+                        }
+                        Console.WriteLine(String.Join(", ", numbersToPrint));
+
+
+                    }
+                    else if (action == "Show")
+                    {
+                        Console.WriteLine(numbers[firstIndex]);
+
+
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exceptionsCount++;
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    exceptionsCount++;
+                }
+                
+
+            }
+            Console.WriteLine(String.Join(", ",numbers));
+        }
+    }
+}
