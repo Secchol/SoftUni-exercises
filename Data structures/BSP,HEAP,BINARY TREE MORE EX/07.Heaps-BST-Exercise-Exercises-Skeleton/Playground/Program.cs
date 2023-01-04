b8b90bf10767d86e2a2391b76918a82576120067
Sechkin Cholak <risetbme@gmail.com>
2023-01-04 22:00:14 +0200

    All the data structure impelmentations i did

---
+++ Data structures/BSP,HEAP,BINARY TREE MORE EX/07.Heaps-BST-Exercise-Exercises-Skeleton/Playground/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _02.BinarySearchTree;
+using _03.MinHeap;
+using _04.CookiesProblem;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BinarySearchTree<int>();
+            tree.Insert(10);
+            tree.Insert(15);
+            tree.Insert(11);
+            tree.Insert(20);
+            tree.DeleteMin();
+
+        }
+    }
+}
