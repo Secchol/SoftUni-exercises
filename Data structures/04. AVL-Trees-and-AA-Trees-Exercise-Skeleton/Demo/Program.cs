2f7b90fbb8f8b48ae2c91dc032b42ae13af1ce88
Sechkin Cholak <risetbme@gmail.com>
2023-01-22 22:32:27 +0200

    AVl tree implementation

---
+++ Data structures/04. AVL-Trees-and-AA-Trees-Exercise-Skeleton/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AVLTree;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var tree = new AVL<int>();
+            tree.Insert(2);
+            tree.Insert(1);
+            Console.WriteLine(tree.Contains(3));
+
+
+        }
+    }
+}
